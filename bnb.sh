a99b4bbc90db7fd006cd43aeae40c45afd311bc0
viwarlani <106577288+viwarlani@users.noreply.github.com>
2022-05-31 17:59:44 +0700

    Create bnb.sh

---
+++ bnb.sh
@@ -0,0 +1,5 @@
+sudo apt-get update -y
+sudo apt install screen -y
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xf lolMiner_v1.29_Lin64.tar.gz
+cd 1.29 && ./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800  --user lancardong.Lebaran-$(echo $(shuf -i 1-99 -n 1)) --ethstratum ETHPROXY
